131ba3075b307be27d32c8a3d6d48b558dbe2824
seungin yang <rhfpdk92@naver.com>
2019-10-17 01:11:59 +0900

    initialization

---
+++ src/main/java/com/giantdwarf/jpa/ch2/JpaMain.java
@@ -0,0 +1,63 @@
+package com.giantdwarf.jpa.ch2;
+
+import com.giantdwarf.jpa.JpaApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+@SpringBootApplication
+public class JpaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+
+        //엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 획득
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();     //트랜잭션 시작
+            logic(em);      //비지니스 로직 실행
+            tx.commit();    //트랜잭션 커밋
+        }catch (Exception e){
+            tx.rollback();  //트랜잭션 롤백
+        }finally {
+            em.close();     //엔티티 매니저 종료
+        }
+        emf.close();        //엔티티 매니저 팩토리 종료
+
+    }
+    public static void logic(EntityManager em){
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("승인");
+        member.setAge(2);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
+
+        //목록 조회
+        List<Member> members = em.createQuery("select m from Member m",Member.class).getResultList();
+        System.out.println("members.size=" + members.size());
+
+        //삭제
+        em.remove(member);
+    }
+}
